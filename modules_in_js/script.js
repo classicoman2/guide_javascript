4fa12043ac4c0e179e4026dd020f6c0491d1c7a5
Classicoman2 <aamengual@iesfbmoll.org>
2020-01-19 15:53:20 +0100

    Update script.js

--- modules_in_js/script.js
+++ modules_in_js/script.js
@@ -1,8 +1,10 @@
-import { helloworld } from './modules/helloworld.js';
+// name of variables and functions go inside curly braces
+import { helloworld, message } from './modules/helloworld.js';
+// Don't need curly braces when using export default in the module
 import helloworlddefault from './modules/helloworlddefault.js'
 
 //Module 'helloworld.js'
-console.log("The variable -> " + helloworld);
+console.log("The variable -> " + message);
 console.log("The function -> " + helloworld());
 
 //Module using 'export default'  - I use the name of the module as a function
