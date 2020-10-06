50ab1d26718a83f6d761a375dd4a75db12fb17bc
T <aamengual@cifpfbmoll.eu>
2020-10-06 11:38:37 +0200

    functions

--- 00_codis_a_classe/test.js
+++ 00_codis_a_classe/test.js
@@ -1,10 +1,23 @@
 
 /**
- * Fa aixo i lo altre
+ * Mira si dos numeros son iguals
  * 
  *  * @param {number} x 
  * @param {number} y 
  */
 function are_equal_numbers(x,y) {
+    //compara els números
+    if (x == y)
+        return true;
+    else
+        return false;
+}
+
+/**
+ * 
+ * @param {*} param1 
+ * @param {*} param2 
+ */
+function whatever(param1, param2) {
 
 }
