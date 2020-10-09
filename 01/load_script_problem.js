70008cf7b09676614d90fa0a875fcad14a6c35c9
toni <aamengual@cifpfbmoll.eu>
2020-10-09 16:41:15 +0200

    loadproblem, etc

---
+++ 01/load_script_problem.js
@@ -0,0 +1,6 @@
+
+window.onload = function () {
+    document.getElementById("text1").innerHTML = "Loaded the script in an external file - FUNCIONA!"
+};
+
+//document.getElementById("text1").innerHTML = "Loaded the script in an external file - FALLA"
