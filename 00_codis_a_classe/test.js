54512213dcf6302f49aed84399c92dd30f9e9d6a
T <aamengual@cifpfbmoll.eu>
2020-10-12 15:54:26 +0200

    tab

--- 00_codis_a_classe/test.js
+++ 00_codis_a_classe/test.js
@@ -1,23 +1,5 @@
+var lastName = "Sanchez";
 
-/**
- * Mira si dos numeros son iguals
- * 
- *  * @param {number} x 
- * @param {number} y 
- */
-function are_equal_numbers(x,y) {
-    //compara els números
-    if (x == y)
-        return true;
-    else
-        return false;
-}
+var lastName;
 
-/**
- * 
- * @param {*} param1 
- * @param {*} param2 
- */
-function whatever(param1, param2) {
-
-}
+console.log(lastName)
