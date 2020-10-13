b675aec144f3a3be74baa5fb1a01a8c5f67cae9d
toni <aamengual@cifpfbmoll.eu>
2020-10-13 19:01:23 +0200

    const, let

--- 01/block-scope-let1.js
+++ 01/block-scope-let1.js
@@ -1,31 +1,17 @@
-
-
-function myFunction() {
-  // LOCAL SCOPE variable using 'var'
-  let var0 = 30;
-  console.log("whatever");
+/**
+ * Exemple 1: empro LET per declarar les VARIABLES LOCALS en les funcions
+ * 
+ * @param {number} a 
+ */
+function suma(a) {
+  let b = 5;
+  return a+b;
 }
 
-//GLOBAL SCOPE variable declared with 'var'
-var var1 = 10;
+/*
+* Exemple 2: puc emplear let en un bucle
+*/
 
-if (var1 == 0) {
-  // BLOCK SCOPE variable declared with 'var'
-  var var2 = 5;
-  let var3 = 10;
+for (let i=0; i<5; i++) {
+  console.log( suma(i) );
 }
-
-try {
-  console.log("The value of var0 is " + var0);
-} catch (e) {
-  console.log("   Error: var0 is not accessible in GLOBAL SCOPE because it is LOCAL");
-}
-
-// var1 was declared in the global scope and is accessible here
-console.log("The value of var1 is " + var1);
-
-// var2 was declared inside a block  {  }  and it is accessible here but not its value
-console.log("The value of var2 is " + var2);
-
-// var3 was declared inside a block  {  }  with let and it is not accessible here
-console.log("The value of var3 is " + var3);
