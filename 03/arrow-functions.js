66bc5495d14ceb27f382924b4457cb0014dbf541
T <aamengual@cifpfbmoll.eu>
2020-10-13 11:33:11 +0200

    arrow functions

--- 03/arrow-functions.js
+++ 03/arrow-functions.js
@@ -1,47 +1,33 @@
-/**
- *  ARROW FUNCIONS
- */
-
-
 
 // Una funció tradicional
 function funcioTradicional(a) {
   return `la funció tradicional retorna ${a}`;
 }
 
-
 // Una funció-variable
 funcioVariable = function (a) {
-  return a;
+  return `la funció variable retorna ${a}`;
 };
 
 
-//Arrow function variable
-arrowFunction = (a) => {
-  return a;
-};
 
-//Arrow function en format comprimit
-myFunction4 = (a) => a;
+// Declaració d'una Arrow function
+funcioArrow = (a,b) => { return `funcioArrow retorna ${a+b}` };
 
-//Cridades a les funcions
-console.log(myFunction1("foo"));
-console.log(myFunction2("goo"));
-console.log(myFunction3("hoo"));
-console.log(myFunction4("koo"));
+//Arrow function en format comprimit (amb 1 sol paràmetre)
+funcioArrowComprimida = (a,b) => { return `funcioArrowComprimida retorna ${a+b}` };
 
-// 2. LA QÜESTIÓ DEL THIS.
 
-document.getElementById("boto1").onclick = function () {
-  console.log(this);
-  console.log(this.id);
-};
 
-document.getElementById("boto2").onclick = () => console.log(this);
 
 
 /**
  * Cridades
  */
 
- funcioTradicional(10);
+console.log( funcioTradicional(10) );
+console.log( funcioVariable(10) );
+
+console.log ( funcioArrow(3,4));
+console.log ( funcioArrowComprimida(3,4));
+
