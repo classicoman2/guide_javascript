e57b37b1d3d0fad718f56fa7d71fd2216e506e39
Toni <aamengual@cifpfbmoll.eu>
2020-10-22 23:43:12 +0200

    08

--- 08/promise-simple.js
+++ 08/promise-simple.js
@@ -1,54 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
+// Declarar la Promesa
+let laMevaPromesa = new Promise(function (resolve, reject) {
+  //Accept
+  resolve("******OK*****");
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Promise Simple</title>
-</head>
+  //Reject
+  //reject("There was an error")
 
-<body>
-
-</body>
-<script>
-    // Declarar la Promesa
-    let laMevaPromesa = new Promise(function (resolve, reject) {
-
-        //Accept
-        resolve("******OK*****");
-
-        //Reject
-        //reject("There was an error")
-
-        /* Cridam resolve(...) quan el que estavem fent
+  /* Cridam resolve(...) quan el que estavem fent
         de forma asincrona s'ha resolt satisfactoriament
            Cridam reject(...) quan ha fallat
         */
 
-        /* En aquest exemple, empram setTimeout(...) per simular un codi
+  /* En aquest exemple, empram setTimeout(...) per simular un codi
         asíncron. En la realitat, fariem normalment una consulta a una API */
-        /*setTimeout(() => {
+  /*setTimeout(() => {
             resolve("La promesa s'ha resolt OK")
             //reject("La promesa ha fallat")
         }, 3000); */
+});
 
-    });
+console.log("---Abans de cridar la promesa---");
 
-    console.log("---Abans de cridar la promesa---");
+//Emprar la Promesa
+laMevaPromesa
+  .then(function (message) {
+    console.log(message);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
-    //Emprar la Promesa
-    laMevaPromesa
-        .then(function (message) {
-            console.log(message)
-        })
-        .catch(function (error) {
-            console.log(error)
-        });
-
-    console.log("---Despres de cridar la promesa---");
-
-
-</script>
-
-</html>
+console.log("---Despres de cridar la promesa---");
