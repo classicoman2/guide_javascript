1af06efc079871f7a4c24df9d0f72ddb40440244
Toni <aamengual@cifpfbmoll.eu>
2020-11-04 17:03:30 +0100

    objectes i fos

--- 03/objectes-i-fos.js
+++ 03/objectes-i-fos.js
@@ -2,7 +2,9 @@
 
 const array1 = [
   { name: "Peter", age: 8 },
+  { name: "Gillian", age: 47 },
   { name: "Nerea", age: 23 },
+  { name: "Stephen", age: 80 },
   { name: "Joe", age: 56 },
   { name: "Claudia", age: 13 },
 ];
@@ -21,11 +23,21 @@ console.log(array2);
  * filter()
  */
 
-console.log( array1 );
+console.log(array1);
 
 let array3 = array1.filter((value) => {
-  return (value.age > 18) ? true : false
+  return value.age > 18 ? true : false;
 });
 
-console.log("Les persones majors d'edat son:");
+console.log("\nLes persones majors d'edat son:");
 console.log(array3);
+
+/**
+ * sort()
+ */
+
+// Ordenar per edat, de menor a major
+let array4 = array1.sort((a, b) => (a.age > b.age ? true : false));
+
+console.log("\nLes persones ordenades de major a menor son:");
+console.log(array4);
