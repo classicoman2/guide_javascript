d4d4bf20ee21043e522e9694263327c3dcef492e
Toni <aamengual@cifpfbmoll.eu>
2020-11-04 16:59:30 +0100

    objectes i fos

--- 03/objectes-i-fos.js
+++ 03/objectes-i-fos.js
@@ -1,15 +1,31 @@
 // Objectes i funcions d'ordre superior.
 
 const array1 = [
-  { name: "Peter", age: 80 },
+  { name: "Peter", age: 8 },
   { name: "Nerea", age: 23 },
   { name: "Joe", age: 56 },
   { name: "Claudia", age: 13 },
 ];
 
-let array2 = array1.map( (value) => {
-    value.age += 100;
-    return value;
-})
+/**
+ * map()
+ */
 
-console.log( array2 );
+let array2 = array1.map((value) => {
+  return value.age + 100;
+});
+
+console.log(array2);
+
+/**
+ * filter()
+ */
+
+console.log( array1 );
+
+let array3 = array1.filter((value) => {
+  return (value.age > 18) ? true : false
+});
+
+console.log("Les persones majors d'edat son:");
+console.log(array3);
