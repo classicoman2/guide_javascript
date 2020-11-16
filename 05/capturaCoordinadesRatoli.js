d71f03d64167ff0b48acd9eb7953c39d4ab215fe
‘classicoman2’ <aamengual@cifpfbmoll.eu>
2020-11-16 19:28:36 +0100

    event click

--- 05/capturaCoordinadesRatoli.js
+++ 05/capturaCoordinadesRatoli.js
@@ -2,8 +2,8 @@ window.onload = function () {
   document
     .getElementById("container")
     .addEventListener("click", function (event) {
-      var x = event.clientX;
-      var y = event.clientY;
+      var x = event.offsetX;
+      var y = event.offsetY;
 
       console.log(`Has clicat a la posicio (${x}, ${y}) del viewport`);
     });
