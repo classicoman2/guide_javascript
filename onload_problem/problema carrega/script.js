c01feb0f5088f690f69c8a19ee58195b40b54bee
‘classicoman2’ <aamengual@cifpfbmoll.eu>
2021-10-15 11:47:57 +0200

    refact: onload problem, explained

---
+++ onload_problem/problema carrega/script.js
@@ -0,0 +1,7 @@
+// Es produirà aquest error
+/*
+Uncaught TypeError: Cannot set properties of null (setting 'innerHTML')
+    at script.js:7
+*/
+
+document.getElementById("missatge").innerHTML = "BONES TARDES!"
---
+++ onload_problem/solució directa problema càrrega/script.js
@@ -0,0 +1,8 @@
+// Evitam l'error controlant la càrrega amb 
+// una callback associada a la propietat  window.onload
+
+window.onload = function () {
+
+    document.getElementById("missatge").innerHTML = "BONES TARDES!"
+
+}
