0f58eff0841cd2532dddac2d3697b0426a06b7d2
‘classicoman2’ <aamengual@cifpfbmoll.eu>
2021-10-21 13:00:37 +0200

    refactor: object examples

--- 03/objectes-basic.js
+++ 03/objectes-basic.js
@@ -1,59 +1,45 @@
 /**
- *  1)- Definir un objecte  bàsic (només propietats)
+ *  1- Objecte  bàsic
  */
-
-{
-  let obj = { name: "Isaac", surname: "Newton", age: 57, married: true };
-
-  console.log(`My name is ${obj.name} ${obj.surname}`);
-  console.log(`I'm ${obj.age} and I'm ${!obj.married ? "not" : ""} married`);
-}
-console.log("");
+let obj = { name: "Isaac", surname: "Newton", age: 57, married: true }
+console.log(`\nMy name is ${obj.name} ${obj.surname}`)
+console.log(`I'm ${obj.age} and I'm ${!obj.married ? "not" : ""} married`)
 
 /**
- *  2)- Array d'objectes
+ *  2- Array d'objectes
  */
-{
-  let persones = [
-    { name: "Maria", surname: "Saal" },
-    { name: "Peter", surname: "Greenway" },
-    { name: "Fran", surname: "Sales" },
-  ];
+let persones = [
+  { name: "Maria", surname: "Saal" },
+  { name: "Peter", surname: "Greenway" },
+  { name: "Fran", surname: "Sales" },
+]
 
-  //Bucle a través d'un array de persones
-  for (p of persones) {
-    console.log(p.name + " " + p.surname);
-  }
+for (p of persones) {
+  console.log(p.name + " " + p.surname)
 }
 
 /**
- *  3)- Objectes compostos
- */
-
-{
-  let grup = {
-    estudis: "DAW",
-    aula: "A208",
-    curs: 2021,
-    alumnes: [
-      { name: "Samantha", surname: "Garcia" },
-      { name: "Peter", surname: "Greenway" },
-      { name: "Andrea", surname: "Tarkovsky" },
-    ],
-  };
-
-  console.log(
-    `\nEl grup de ${grup.estudis} en el curs ${grup.curs} ocupa l'aula ${grup.aula} 
-    i els seus alumnes
-     son ${grup.alumnes[0].name}, ${grup.alumnes[1].name} i ${grup.alumnes[2].name}`
-  );
-}
-
-/**
- *  4)- Mètodes d'un objecte
- *  Cal emprar 'this' per accedir a propietats i mètodes des de dins dels mètodes.
+ *  3- Objecte compost
  */
 let grup = {
+  estudis: "DAW",
+  aula: "A208",
+  curs: 2021,
+  alumnes: [
+    { name: "Samantha", surname: "Garcia" },
+    { name: "Peter", surname: "Greenway" },
+    { name: "Andrea", surname: "Tarkovsky" },
+  ],
+}
+console.log(
+  `\nEl grup ${grup.estudis} dels curs ${grup.curs} ocupa l'aula ${grup.aula}. Alumnat: ${grup.alumnes[0].name}, ${grup.alumnes[1].name} i ${grup.alumnes[2].name}`
+)
+
+/**
+ *  4- Propietats i Mètodes d'un objecte
+ *  Cal emprar 'this' per accedir a propietats i mètodes des de dins dels mètodes.
+ */
+grup = {
   //PROPIETATS
   estudis: "DAW",
   aula: "A208",
@@ -65,56 +51,50 @@ let grup = {
 
   //MÈTODES
   recompteAlumnes: function () {
-    return this.alumnes.length;
+    return this.alumnes.length
   },
 
   getAlumnName: function (i) {
-    return this.alumnes[i].name;
+    return this.alumnes[i].name
   },
 
   getAlumnSurname: function (i) {
-    return this.alumnes[i].surname;
+    return this.alumnes[i].surname
   },
   getAlumnNomComplet: function (i) {
-    return this.getAlumnName(i) + " " + this.getAlumnSurname(i);
+    return this.getAlumnName(i) + " " + this.getAlumnSurname(i)
   },
-};
+}
 
-console.log(`\nEn l'aula hi ha ${grup.recompteAlumnes()} alumnes`);
-console.log(`L'alumne número 1 es ${grup.getAlumnNomComplet(1)}`);
+console.log(`\nEn l'aula hi ha ${grup.recompteAlumnes()} alumnes`)
+console.log(`L'alumne número 1 es ${grup.getAlumnNomComplet(1)}`)
 
 /**
- *  5)- Afegir propietats i mètodes després de crear un objecte
+ *  5- Afegir propietats i mètodes després de crear un objecte
  */
 
 //Afegeix propietat
-grup.pis = "1er";
+grup.pis = "1er"
 
 //Afegeix mètodes
 grup.CanviaNomAlumne = function (i, nom) {
-  grup.alumnes[i].name = nom;
-};
+  grup.alumnes[i].name = nom
+}
 
-grup.CanviaLlinatgeAlumne = function (i, llinatge) {
-  grup.alumnes[i].surname = llinatge;
-};
-
-grup.CanviaNomAlumne(1, "Ernest");
-grup.CanviaLlinatgeAlumne(1, "Stanton");
-console.log(`L'aula del grup és la ${grup.aula} en el ${grup.pis} pis`);
-console.log(`\nL'alumne número 1 es ${grup.getAlumnNomComplet(1)}`);
+grup.CanviaNomAlumne(1, "Ernest")
+console.log(`\nL'aula del grup és la ${grup.aula} en el ${grup.pis} pis`)
+console.log(`L'alumne número 1 es ${grup.getAlumnNomComplet(1)}`)
 
 /**
  *  6)- Els objectes son mutables
  */
-var x = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" };
+var x = { name: "John", surname: "Doe", age: 50 }
 
-var y = x;
-y.firstName = "Marlon";
-y.age = 10; // This will change both x.age and person.age
+var y = x
+y.name = "Marlon"
+y.age = 10 // This will change both x.age and person.age
 
-console.log("");
-console.log("x = ");
-console.log(x);
-console.log("y = ");
-console.log(y);
+console.log("\nx = ")
+console.log(x)
+console.log("y = ")
+console.log(y)
--- 03/objectes-prototip.js
+++ 03/objectes-prototip.js
@@ -1,20 +1,25 @@
 // Constructor function for Person objects
 function Person(first, last, age) {
-  this.firstName = first;
-  this.lastName = last;
-  this.age = age;
+  this.firstName = first
+  this.lastName = last
+  this.age = age
 }
 
+let dog = { name: "Rambo", weight: 8.5 }
+
 // Create a Person object
-var trump = new Person('Donald', 'Trump', 74);
-var biden = new Person('Joe', 'Biden', 78);
+var trump = new Person("Donald", "Trump", 74)
+var biden = new Person("Joe", "Biden", 78)
 
-//Afegir una propietat al prototip
-Person.prototype.country = 'US';
-
-//Afegir un mètode al prototip
+//Afegir una propietat i un mètode al prototip de Person
+Person.prototype.country = "US"
 Person.prototype.getFullName = function () {
-  return `${this.firstName} ${this.lastName}`;
-};
+  return `${this.firstName} ${this.lastName}`
+}
 
-console.log(` ${trump.getFullName()} is from ${trump.country}`);
+console.log(` ${trump.getFullName()} is from ${trump.country}`)
+
+// Afegir una propietat al prototip de Dog
+//dog.prototype.age = 5;
+
+console.log("Dog has "+dog.age + " years")
