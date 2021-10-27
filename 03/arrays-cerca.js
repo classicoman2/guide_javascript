3bfc0901cfb4f1473b1cb352e058812b28b4e28b
‘classicoman2’ <aamengual@cifpfbmoll.eu>
2021-10-27 16:15:06 +0200

    refact: arrays

--- 03/arrays-cerca.js
+++ 03/arrays-cerca.js
@@ -13,14 +13,27 @@ var array2 = [100, 25, -1, 0.78, -50, 5, 0.5]
 let first_r = array1.find((value) => value.indexOf("r") != -1)
 console.log("find() - el 1er element amb una r és: " + first_r)
 
+let first4 = array1.find((value) => value.length == 4)
+console.log("find() - el 1er element amb longitut igual a 4 es: " + first4)
+
 let firstNegative = array2.find((value) => value < 0)
 console.log("find() - el 1er element negatiu és: " + firstNegative)
+
+console.log("\n")
 
 /**
  * findIndex()
  */
 let posFirst_r = array1.findIndex((value) => value.indexOf("r") != -1)
-console.log("findIndex() - la posicio del 1er element amb una r és: " + posFirst_r)
+console.log(
+  "findIndex() - la posicio del 1er element amb una r és: " + posFirst_r
+)
+
+let posFirst = array1.findIndex((value) => value.length == 4)
+console.log(
+  "findIndex() - la posició del 1er element amb longitut igual a 4 es: " +
+    posFirst
+)
 
 let posFirstNegative = array2.findIndex((value) => value < 0)
 console.log(
