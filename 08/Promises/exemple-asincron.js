134d7c153a01c0b311bffc6a78002d0017257dfb
toni <aamengual@cifpfbmoll.eu>
2022-10-27 19:24:28 +0200

    afegit exemple biblioteca.html i altres

--- 08/Promises/exemple-asincron.js
+++
@@ -1,9 +0,0 @@
-
-console.log("ADALT")
-
-// ASINCRON
-setTimeout(() => {
-    console.log("TIMER HA ACABAT")
-}, 0);
-
-console.log("ABAIX")
---
+++ 08/exemple-asincron.js
@@ -0,0 +1,8 @@
+console.log('ADALT')
+
+// ASINCRON
+setTimeout(function () {
+  console.log('TIMER HA ACABAT')
+}, 0)
+
+for (i = 0; i < 10000; i++) console.log('ABAIX')
